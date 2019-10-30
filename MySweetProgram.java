e18937d9cc40852f442decf79d0d3a5b4a93a188
BETeng <brian7.teng@gmail.com>
2019-10-29 19:46:36 -0500

    basic class creation

---
+++ MySweetProgram.java
@@ -0,0 +1,11 @@
+public class MySweetProgram {
+  
+  public static void main(String[] args){
+    User user = new User();
+    user.firstName = "Brian";
+    user.lastName = "Teng";
+
+    System.out.println(user.firstName);
+    System.out.println(user.lastName);
+  }
+}
---
+++ User.java
@@ -0,0 +1,4 @@
+public class User {
+  public String firstName;
+  public String lastName;
+}
