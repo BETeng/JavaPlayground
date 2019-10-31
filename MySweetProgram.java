46b8af10092b25308f681e70c8932e0ef75b3917
BETeng <brian7.teng@gmail.com>
2019-10-30 23:15:04 -0500

    println with ojbects

--- MySweetProgram.java
+++ MySweetProgram.java
@@ -4,20 +4,19 @@ import java.util.List;
 public class MySweetProgram {
   
   public static void main(String[] args){
+    String[] firstNames = {"abby", "ben", "charles", "dick"};
+    String[] lastNames = {"alonzo", "boof", "chap", "davids"};
 
-    User me = new User();
-    me.setFirstName("Brian");
-    me.setLastName("Teng");
-
-    User you = new User();
-    you.setFirstName("chump");
-    you.setLastName("change");
-
-    
- List<User> users = new ArrayList<User>();
- users.add(you);
- users.add(me);
-
- System.out.println(users.get(0).getFullName());
+    List<User> users = new ArrayList<User>();
+ 
+    for(int i = 0; i < firstNames.length; i++){
+      User u = new User();
+      u.setFirstName(firstNames[i]);
+      u.setLastName(lastNames[i]);
+      users.add(u);
+    }
+    for(User u : users) {
+      System.out.println(u.getFullName());
+    }
   }
 }
