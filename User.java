d983a1373342257143160aaf7cc100f9955f60ff
BETeng <brian7.teng@gmail.com>
2019-11-04 18:39:18 -0600

    abstract class requiring method override

--- User.java
+++ User.java
@@ -1,11 +1,9 @@
 import java.util.List;
 
-public class User {
+public abstract class User {
   public String firstName;
   public String lastName;
   public boolean verified = false;
 
-  public void sayHello(){
-    System.out.println("hi, im a user. my name is " + firstName+" " + lastName);
-  }
+  public abstract void sayHello();
 }
